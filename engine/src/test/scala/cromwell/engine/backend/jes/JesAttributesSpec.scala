6b75030ae1db1c76493a29d3284ecff434379eed
Ruchi Munshi <rmunshi@broadinstitute.org>
2016-06-29 13:27:37 -0400

    move out of jesbackedSpec

--- engine/src/test/scala/cromwell/engine/backend/jes/JesAttributesSpec.scala
+++ engine/src/test/scala/cromwell/engine/backend/jes/JesAttributesSpec.scala
@@ -3,12 +3,63 @@ package cromwell.engine.backend.jes
 import java.net.URL
 
 import com.typesafe.config.ConfigFactory
+import cromwell.CromwellTestkitSpec
 import cromwell.backend.BackendConfigurationDescriptor
-import cromwell.filesystems.gcs.GoogleConfiguration
-import org.scalatest.{FlatSpec, Matchers}
+import cromwell.backend.impl.jes.{JesAttributes, JesBackendLifecycleActorFactory}
+import cromwell.backend.impl.jes.io.{DiskType, JesWorkingDisk}
+import cromwell.core.WorkflowOptions
+import cromwell.engine.workflow.WorkflowDescriptorBuilder
+import cromwell.filesystems.gcs.{ApplicationDefaultMode, RefreshTokenMode, GoogleConfiguration}
+import cromwell.util.EncryptionSpec
+import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
+import org.specs2.mock.Mockito
 import wdl4s.ThrowableWithErrors
 
-class JesAttributesSpec extends FlatSpec with Matchers {
+class JesAttributesSpec extends FlatSpec with Matchers with Mockito with BeforeAndAfterAll with WorkflowDescriptorBuilder {
+
+  val testWorkflowManagerSystem = new CromwellTestkitSpec.TestWorkflowManagerSystem()
+  override implicit val actorSystem = testWorkflowManagerSystem.actorSystem
+  val workingDisk = JesWorkingDisk(DiskType.SSD, 200)
+
+  override protected def afterAll() = {
+    testWorkflowManagerSystem.shutdownTestActorSystem()
+    super.afterAll()
+  }
+
+  val clientSecrets = RefreshTokenMode(name = "bar", clientId = "secret-id", clientSecret = "secret-secret")
+  val backendConfigDescriptor = new BackendConfigurationDescriptor(CromwellTestkitSpec.JesBackendConfig, CromwellTestkitSpec.DefaultConfig)
+  val jesBackend = new JesBackendLifecycleActorFactory(backendConfigDescriptor)
+
+  override val anyString = ""
+  val anyURL: URL = null
+
+  val jesAttributes = new JesAttributes(
+    project = anyString,
+    genomicsAuth = ApplicationDefaultMode(name = "foo"),
+    gcsFilesystemAuth = clientSecrets,
+    executionBucket = anyString,
+    endpointUrl = anyURL,
+    maxPollingInterval = 600)
+
+  it should "be verified when localizing with Refresh Token" in {
+    EncryptionSpec.assumeAes256Cbc()
+
+    val goodOptions = WorkflowOptions.fromMap(Map("refresh_token" -> "token")).get
+
+    try {
+      jesAttributes.assertWorkflowOptions(goodOptions)
+    } catch {
+      case e: IllegalArgumentException => fail("Correct options validation should not throw an exception.")
+      case t: Throwable =>
+        t.printStackTrace()
+        fail(s"Unexpected exception: ${t.getMessage}")
+    }
+
+    val missingToken = WorkflowOptions.fromMap(Map.empty).get
+    the [IllegalArgumentException] thrownBy {
+      jesAttributes.assertWorkflowOptions(missingToken)
+    } should have message s"Missing parameters in workflow options: refresh_token"
+  }
 
   it should "parse correct JES config" in {
 //    val configString = """
@@ -50,25 +101,25 @@ class JesAttributesSpec extends FlatSpec with Matchers {
 //    noPreemptibleAttributes.maxPollingInterval shouldBe 600
 //  }
 //
-//  it should "not parse invalid config" in {
-//    val nakedConfig =
-//      ConfigFactory.parseString("""
-//        |{
-//        |   genomics {
-//        |     endpoint-url = "myEndpoint"
-//        |   }
-//        |}
-//      """.stripMargin)
-//
-//    val googleConfig = GoogleConfiguration(ConfigFactory.load)
-//
-//    val exception = intercept[IllegalArgumentException with ThrowableWithErrors] {
-//      JesAttributes.apply(googleConfig, BackendConfigurationDescriptor(nakedConfig, ConfigFactory.load))
-//    }
-//    val errorsList = exception.errors.list
-//    errorsList should contain ("Could not find key: project")
-//    errorsList should contain ("Could not find key: root")
-//    errorsList should contain ("no protocol: myEndpoint")
+  it should "not parse invalid config" in {
+    val nakedConfig =
+      ConfigFactory.parseString("""
+        |{
+        |   genomics {
+        |     endpoint-url = "myEndpoint"
+        |   }
+        |}
+      """.stripMargin)
+
+    val googleConfig = GoogleConfiguration(ConfigFactory.load)
+
+    val exception = intercept[IllegalArgumentException with ThrowableWithErrors] {
+      JesAttributes.apply(googleConfig, nakedConfig)
+    }
+    val errorsList = exception.errors.list
+    errorsList should contain ("Could not find key: project")
+    errorsList should contain ("Could not find key: root")
+    errorsList should contain ("no protocol: myEndpoint")
   }
 
 }
--- engine/src/test/scala/cromwell/engine/backend/jes/JesBackendSpec.scala
+++ engine/src/test/scala/cromwell/engine/backend/jes/JesBackendSpec.scala
@@ -38,20 +38,17 @@ class JesBackendSpec extends FlatSpec with Matchers with Mockito with BeforeAndA
   }
 
   val clientSecrets = RefreshTokenMode(name = "bar", clientId = "secret-id", clientSecret = "secret-secret")
-  val backendConfigDescriptor = new BackendConfigurationDescriptor(CromwellTestkitSpec.JesBackendConfig, CromwellTestkitSpec.DefaultConfig)
-  val jesBackend = new JesBackendLifecycleActorFactory(backendConfigDescriptor)
-
-  override val anyString = ""
-  val anyURL: URL = null
-
-  val jesAttributes = new JesAttributes(
-    project = anyString,
-    genomicsAuth = ApplicationDefaultMode(name = "foo"),
-    gcsFilesystemAuth = clientSecrets,
-    executionBucket = anyString,
-    endpointUrl = anyURL,
-    maxPollingInterval = 600)
-
+  //  val jesBackend = new OldStyleJesBackend(CromwellTestkitSpec.JesBackendConfigEntry, actorSystem) {
+  //    private val anyString = ""
+  //    private val anyURL: URL = null
+  //    override lazy val jesAttributes = new JesAttributes(
+  //      project = anyString,
+  //      executionBucket = anyString,
+  //      endpointUrl = anyURL,
+  //      maxPollingInterval = 600,
+  //      genomicsAuth = ApplicationDefaultMode(name = "foo"),
+  //      gcsFilesystemAuth = clientSecrets)
+  //  }
   ignore should "handle Failure Status" in {
   //"executionResult" should "handle Failure Status" in {
 //    import scala.concurrent.ExecutionContext.Implicits.global
@@ -171,27 +168,27 @@ class JesBackendSpec extends FlatSpec with Matchers with Mockito with BeforeAndA
 
   //"workflow options existence" should "be verified when localizing with Refresh Token" in {
   it should "be verified when localizing with Refresh Token" in {
-    EncryptionSpec.assumeAes256Cbc()
-
-    val goodOptions = WorkflowOptions.fromMap(Map("refresh_token" -> "token")).get
-
-    try {
-      jesAttributes.assertWorkflowOptions(goodOptions)
-    } catch {
-      case e: IllegalArgumentException => fail("Correct options validation should not throw an exception.")
-      case t: Throwable =>
-        t.printStackTrace()
-        fail(s"Unexpected exception: ${t.getMessage}")
-    }
-
-    val missingToken = WorkflowOptions.fromMap(Map.empty).get
-    the [IllegalArgumentException] thrownBy {
-      jesAttributes.assertWorkflowOptions(missingToken)
-    } should have message s"Missing parameters in workflow options: refresh_token"
+//    EncryptionSpec.assumeAes256Cbc()
+//
+//    val goodOptions = WorkflowOptions.fromMap(Map("refresh_token" -> "token")).get
+//
+//    try {
+//      jesAttributes.assertWorkflowOptions(goodOptions)
+//    } catch {
+//      case e: IllegalArgumentException => fail("Correct options validation should not throw an exception.")
+//      case t: Throwable =>
+//        t.printStackTrace()
+//        fail(s"Unexpected exception: ${t.getMessage}")
+//    }
+//
+//    val missingToken = WorkflowOptions.fromMap(Map.empty).get
+//    the [IllegalArgumentException] thrownBy {
+//      jesAttributes.assertWorkflowOptions(missingToken)
+//    } should have message s"Missing parameters in workflow options: refresh_token"
   }
 
   ignore should "create a GcsAuthInformation instance" in {
-//    val workflowDescriptor = mock[OldStyleWorkflowDescriptor]
+//    val workflowDescriptor = mock[BackendWorkflowDescriptor]
 //    val mockedWfOptions = mock[WorkflowOptions]
 //    workflowDescriptor.workflowOptions returns mockedWfOptions
 //    mockedWfOptions.get("refresh_token") returns Success("myRefreshToken")
